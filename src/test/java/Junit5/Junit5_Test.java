3c4d2edb5cc8b50d1c406d7d2f8d3ee4e88c0bae
AfroozPoorhashemi <afrooz1353@yahoo.com>
2021-07-03 12:01:17 -0400

    Initial Commit

---
+++ src/test/java/Junit5/Junit5_Test.java
@@ -0,0 +1,13 @@
+package Junit5;
+
+import org.junit.jupiter.api.Test;
+
+public class Junit5_Test {
+
+    @Test
+    public void test(){
+        System.out.println("Setup new project");
+    }
+
+
+}
