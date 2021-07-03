e6ba951d613a461dda32f590273f00a1fdeff7d8
AfroozPoorhashemi <afrooz1353@yahoo.com>
2021-07-03 15:24:05 -0400

    Adding static import for Assertions and failing test

---
+++ src/test/java/Junit5/Check.java
@@ -0,0 +1,24 @@
+package Junit5;
+
+import io.restassured.RestAssured;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.baseURI;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.jupiter.api.*;
+
+
+import static io.restassured.RestAssured.get;
+
+public class Check {
+
+    @Test
+    public void PetStore(){
+
+   RestAssured.baseURI  = "https://petstore.swagger.io";
+   RestAssured.basePath = "/api";
+
+   get("/swagger").prettyPeek();
+    }
+}
