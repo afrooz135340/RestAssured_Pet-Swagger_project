774de4024f96d502726411299510545870ca6787
AfroozPoorhashemi <afrooz1353@yahoo.com>
2021-07-03 12:50:52 -0400

    Adding static import for Assertions

--- src/test/java/Junit5/Junit5_Test.java
+++ src/test/java/Junit5/Junit5_Test.java
@@ -2,12 +2,18 @@ package Junit5;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class Junit5_Test {
 
     @Test
     public void test(){
         System.out.println("Setup new project");
+        assertEquals(1,2);
+    }
+
     }
 
 
-}
+
